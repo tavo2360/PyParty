ff96eb63170ae35c995788fba026b6d4af626413
tavo2360 <tavo.2360@yahoo.com>
2014-05-13 21:20:21 -0600

    begining window and function that creates button for reference our game will be made in english

---
+++ pyparty.py
@@ -0,0 +1,47 @@
+import sys
+import pygame,math,cmath
+from pygame.locals import *
+fractal_level = 4
+# ==========================================================================
+# Color's definition
+# ==========================================================================
+
+yellow=(255,238,0)									  
+blue=(0,107,179)
+darkBlue=(24,41,131)
+lightBlue=(0,157,226)
+fuchsia=(255,0,128)
+purple=(147,17,126)
+orange=(255,153,51)
+lightRed=(227,0,79)
+red=(227,0,35)
+pink=(225,0,122)
+lightGreen=(151,190,13)
+green=(0,145,54)
+white=(255,255,255)
+black=(0,0,0)
+
+# ==========================================================================			
+# Function to create a window
+# ==========================================================================
+def window(x, y):									  # Creates window where program runs
+	pygame.init()									  
+	screen = pygame.display.set_mode((x,y))
+	background = pygame.image.load("background.jpg").convert()		#opens background image
+	pygame.display.set_caption("Figure Maker ADVANCED 2")
+	screen.blit(background,(0,0))
+	pygame.display.update()	   
+	return screen
+	
+
+# ==========================================================================
+# Function to create buttons
+# ==========================================================================
+def Button(screen, x, y, width, text, buttonColor, textColor):	   #creats buttons
+	font=pygame.font.Font(None,24)								   #choses font size
+	rect=pygame.draw.rect(screen, black, (x,y,width,30), 1)		   #button shape
+	screen.fill(buttonColor, rect)
+	screen.blit(font.render(text, True, textColor), (x+20, y+7))
+	pygame.display.update()
+	return [rect, text]
+
