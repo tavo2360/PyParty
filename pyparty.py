0089821d6e0c9590ea30b061a7788090ac465fae
Wilopez <Wilopez95@hotmail.com>
2014-05-18 11:58:03 -0600

    Wilson
    cree la ventana 1 y intente poner sonido

--- pyparty.py
+++ pyparty.py
@@ -1,6 +1,37 @@
 import sys
 import pygame,math,cmath
 from pygame.locals import *
+flag=True
+
+def Windons():
+  pygame.init()
+  screen = pygame.display.set_mode((1024,600))
+  background = pygame.image.load("background.jpg").convert()
+  pygame.mixer.music.load("presentation.mp3")
+  pygame.display.set_caption("PyPARTY")
+  screen.blit(background,(0,0))
+  pygame.display.update()
+  return screen
+  pygame.mixer.music.play(2)
+  while (flag):
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+             pygame.quit(); sys.exit();
+
+Windons()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # ==========================================================================
 # Color's definition
@@ -20,28 +51,28 @@ lightGreen=(151,190,13)
 green=(0,145,54)
 white=(255,255,255)
 black=(0,0,0)
-
-# ==========================================================================			
-# Function to create a window
-# ==========================================================================
-def window(x, y):									  # Creates window where program runs
-	pygame.init()									  
-	screen = pygame.display.set_mode((x,y))
-	background = pygame.image.load("background.jpg").convert()		#opens background image
-	pygame.display.set_caption("Figure Maker ADVANCED 2")
-	screen.blit(background,(0,0))
-	pygame.display.update()	   
-	return screen
-	
-
-# ==========================================================================
-# Function to create buttons
-# ==========================================================================
-def Button(screen, x, y, width, text, buttonColor, textColor):	   #creats buttons
-	font=pygame.font.Font(None,24)								   #choses font size
-	rect=pygame.draw.rect(screen, black, (x,y,width,30), 1)		   #button shape
-	screen.fill(buttonColor, rect)
-	screen.blit(font.render(text, True, textColor), (x+20, y+7))
-	pygame.display.update()
-	return [rect, text]
+##
+### ==========================================================================			
+### Function to create a window
+### ==========================================================================
+##def window(x, y):									  # Creates window where program runs
+##	pygame.init()									  
+##	screen = pygame.display.set_mode((x,y))
+##	background = pygame.image.load("background.jpg").convert()		#opens background image
+##	pygame.display.set_caption("Figure Maker ADVANCED 2")
+##	screen.blit(background,(0,0))
+##	pygame.display.update()	   
+##	return screen
+##	
+##
+### ==========================================================================
+### Function to create buttons
+### ==========================================================================
+##def Button(screen, x, y, width, text, buttonColor, textColor):	   #creats buttons
+##	font=pygame.font.Font(None,24)								   #choses font size
+##	rect=pygame.draw.rect(screen, black, (x,y,width,30), 1)		   #button shape
+##	screen.fill(buttonColor, rect)
+##	screen.blit(font.render(text, True, textColor), (x+20, y+7))
+##	pygame.display.update()
+##	return [rect, text]
 
