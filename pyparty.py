022a8f2f73a66b06e29f45c779e2d5b7f95d1dca
tavo2360 <tavo.2360@yahoo.com>
2014-05-18 16:07:23 -0600

    tablero completo
    finished the board

--- pyparty.py
+++ pyparty.py
@@ -21,18 +21,6 @@ def Windons():
 Windons()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 # ==========================================================================
 # Color's definition
 # ==========================================================================
@@ -51,7 +39,7 @@ lightGreen=(151,190,13)
 green=(0,145,54)
 white=(255,255,255)
 black=(0,0,0)
-<<<<<<< HEAD
+
 
 # ==========================================================================			
 # Function to create a window
@@ -81,75 +69,75 @@ def Button(screen, x, y, width, text, buttonColor, textColor):	   #creats button
 # Function to create board 
 # ==========================================================================
 
-def boardsquares(x,y,width,squarecolor,interval,startcolor,orientation):
+def boardsquares(x,y,width,squarecolor,interval,startcolor,orientation,border,bordercolor):
 	square=pygame.draw.rect(screen,
 					 squarecolor,
 					 (x,y,width,width),0)
 	square=pygame.draw.rect(screen,
-					 black,
-					 (x,y,width,width),2)
+					 bordercolor,
+					 (x,y,width,width),border)
 	if orientation=="horizontal":
 		if startcolor==blue:
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+width,y,width,green,interval,startcolor,orientation)
+				boardsquares(x+width,y,width,green,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+2*width,y,width,red,interval,startcolor,orientation)
+				boardsquares(x+2*width,y,width,red,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+3*width,y,width,blue,interval,startcolor,orientation)
+				boardsquares(x+3*width,y,width,blue,interval,startcolor,orientation,border,bordercolor)
 		if startcolor==green:
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+width,y,width,red,interval,startcolor,orientation)
+				boardsquares(x+width,y,width,red,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+2*width,y,width,blue,interval,startcolor,orientation)
+				boardsquares(x+2*width,y,width,blue,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+3*width,y,width,green,interval,startcolor,orientation)
+				boardsquares(x+3*width,y,width,green,interval,startcolor,orientation,border,bordercolor)
 		if startcolor==red:
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+width,y,width,blue,interval,startcolor,orientation)
+				boardsquares(x+width,y,width,blue,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+2*width,y,width,green,interval,startcolor,orientation)
+				boardsquares(x+2*width,y,width,green,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x+3*width,y,width,red,interval,startcolor,orientation)
+				boardsquares(x+3*width,y,width,red,interval,startcolor,orientation,border,bordercolor)
 	if orientation=="vertical":
 		if startcolor==blue:
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+width,width,green,interval,startcolor,orientation)
+				boardsquares(x,y+width,width,green,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+2*width,width,red,interval,startcolor,orientation)
+				boardsquares(x,y+2*width,width,red,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+3*width,width,blue,interval,startcolor,orientation)
+				boardsquares(x,y+3*width,width,blue,interval,startcolor,orientation,border,bordercolor)
 		if startcolor==green:
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+width,width,red,interval,startcolor,orientation)
+				boardsquares(x,y+width,width,red,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+2*width,width,blue,interval,startcolor,orientation)
+				boardsquares(x,y+2*width,width,blue,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+3*width,width,green,interval,startcolor,orientation)
+				boardsquares(x,y+3*width,width,green,interval,startcolor,orientation,border,bordercolor)
 		if startcolor==red:
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+width,width,blue,interval,startcolor,orientation)
+				boardsquares(x,y+width,width,blue,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+2*width,width,green,interval,startcolor,orientation)
+				boardsquares(x,y+2*width,width,green,interval,startcolor,orientation,border,bordercolor)
 			if interval > 0:
 				interval -= 1
-				boardsquares(x,y+3*width,width,red,interval,startcolor,orientation)
+				boardsquares(x,y+3*width,width,red,interval,startcolor,orientation,border,bordercolor)
 
 # ==========================================================================
 # Function to create board window
@@ -161,9 +149,28 @@ def boardwindow():
 	screen = window(749, 468) 
 	pygame.display.set_caption("PyParty Board")
 	screen.fill(black)
-	boardsquares(150,20,50,blue,6,blue,"horizontal")
-	boardsquares(150,370,50,blue,6,blue,"horizontal")
-	boardsquares(500,20,50,green,7,green,"vertical")
+	boardsquares(200,20,50,blue,5,blue,"horizontal",2,black)
+	boardsquares(156,22,46,blue,0,blue,"horizontal",2,blue)
+	boardsquares(116,22,46,blue,0,blue,"horizontal",2,blue)
+	boardsquares(100,22,46,blue,0,blue,"horizontal",2,blue)
+	boardsquares(100,60,46,blue,0,blue,"horizontal",2,blue)
+	boardsquares(100,95,46,blue,0,blue,"horizontal",2,blue)
+	boardsquares(500,370,-50,green,6,green,"horizontal",2,black)
+	boardsquares(500,20,50,blue,6,blue,"vertical",2,black)
+	boardsquares(150,370,-50,red,4,red,"vertical",2,black)
+	boardsquares(100,95,46,blue,0,blue,"horizontal",2,blue)
+	#shortcut5 to 20
+	boardsquares(400,70,50,red,3,red,"vertical",8,yellow)
+	boardsquares(400,270,-50,blue,4,blue,"horizontal",8,yellow)
+	boardsquares(150,270,50,red,0,red,"horizontal",8,yellow)
+	#shortcut5 to 15
+	boardsquares(415,85,20,red,0,red,"vertical",8,purple)
+	boardsquares(400,120,-50,blue,2,blue,"horizontal",8,purple)
+	boardsquares(250,120,50,blue,3,blue,"vertical",8,purple)
+	boardsquares(300,270,50,green,1,green,"horizontal",8,purple)
+	boardsquares(400,270,50,red,0,red,"horizontal",8,purple)
+	#shorcut10 to 12
+	boardsquares(550,170,50,blue,2,blue,"vertical",8,white)
 	pygame.display.flip()
 	while done:
 		for event in pygame.event.get(): # User did something
@@ -178,30 +185,5 @@ boardwindow()
 
 
 
-=======
-##
-### ==========================================================================			
-### Function to create a window
-### ==========================================================================
-##def window(x, y):									  # Creates window where program runs
-##	pygame.init()									  
-##	screen = pygame.display.set_mode((x,y))
-##	background = pygame.image.load("background.jpg").convert()		#opens background image
-##	pygame.display.set_caption("Figure Maker ADVANCED 2")
-##	screen.blit(background,(0,0))
-##	pygame.display.update()	   
-##	return screen
-##	
-##
-### ==========================================================================
-### Function to create buttons
-### ==========================================================================
-##def Button(screen, x, y, width, text, buttonColor, textColor):	   #creats buttons
-##	font=pygame.font.Font(None,24)								   #choses font size
-##	rect=pygame.draw.rect(screen, black, (x,y,width,30), 1)		   #button shape
-##	screen.fill(buttonColor, rect)
-##	screen.blit(font.render(text, True, textColor), (x+20, y+7))
-##	pygame.display.update()
-##	return [rect, text]
->>>>>>> 0089821d6e0c9590ea30b061a7788090ac465fae
+
 
