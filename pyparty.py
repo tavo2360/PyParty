c6070d7a54de6ae455013ed3fa2fd0aa1880f20d
Wilopez <Wilopez95@hotmail.com>
2014-05-18 15:28:18 -0600

    Wilson
    Actualizado

--- pyparty.py
+++ pyparty.py
@@ -7,18 +7,25 @@ def Windons():
   salir=False
   pygame.init()
   screen = pygame.display.set_mode((1024,600))
-  background = pygame.image.load("background.jpg").convert()
+  background = pygame.image.load("fondo.jpg").convert()
   pygame.mixer.music.load("presentation.mp3")
   pygame.display.set_caption("PyPARTY")
+  #pygame.display.set_icon("icon.jpg")  
   screen.blit(background,(0,0))
+  imagen1=pygame.image.load("gif.png")
+  screen.blit(imagen1,(300,500))
   pygame.display.update()
   pygame.mixer.music.play(2)
   while salir!= True:
       for event in pygame.event.get():
           if event.type == pygame.QUIT:
-            salir=True
-            
-  pygame.quit()
+              salir=True
+          if event.type == pygame.KEYDOWN:
+                 print("Hola")
+               
+#            
+  pygame.quit()          
+  
 Windons()
 
 
@@ -48,134 +55,6 @@ lightGreen=(151,190,13)
 green=(0,145,54)
 white=(255,255,255)
 black=(0,0,0)
-<<<<<<< HEAD
-
-# ==========================================================================			
-# Function to create a window
-# ==========================================================================
-def window(x, y):									  # Creates window where program runs
-	pygame.init()									  
-	screen = pygame.display.set_mode((x,y))
-	background = pygame.image.load("background.jpg").convert()		#opens background image
-	pygame.display.set_caption("Figure Maker ADVANCED 2")
-	screen.blit(background,(0,0))
-	pygame.display.update()	   
-	return screen
-	
-
-# ==========================================================================
-# Function to create buttons
-# ==========================================================================
-def Button(screen, x, y, width, text, buttonColor, textColor):	   #creats buttons
-	font=pygame.font.Font(None,24)								   #choses font size
-	rect=pygame.draw.rect(screen, black, (x,y,width,30), 1)		   #button shape
-	screen.fill(buttonColor, rect)
-	screen.blit(font.render(text, True, textColor), (x+20, y+7))
-	pygame.display.update()
-	return [rect, text]
-	
-# ==========================================================================
-# Function to create board 
-# ==========================================================================
-
-def boardsquares(x,y,width,squarecolor,interval,startcolor,orientation):
-	square=pygame.draw.rect(screen,
-					 squarecolor,
-					 (x,y,width,width),0)
-	square=pygame.draw.rect(screen,
-					 black,
-					 (x,y,width,width),2)
-	if orientation=="horizontal":
-		if startcolor==blue:
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+width,y,width,green,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+2*width,y,width,red,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+3*width,y,width,blue,interval,startcolor,orientation)
-		if startcolor==green:
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+width,y,width,red,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+2*width,y,width,blue,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+3*width,y,width,green,interval,startcolor,orientation)
-		if startcolor==red:
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+width,y,width,blue,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+2*width,y,width,green,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x+3*width,y,width,red,interval,startcolor,orientation)
-	if orientation=="vertical":
-		if startcolor==blue:
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+width,width,green,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+2*width,width,red,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+3*width,width,blue,interval,startcolor,orientation)
-		if startcolor==green:
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+width,width,red,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+2*width,width,blue,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+3*width,width,green,interval,startcolor,orientation)
-		if startcolor==red:
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+width,width,blue,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+2*width,width,green,interval,startcolor,orientation)
-			if interval > 0:
-				interval -= 1
-				boardsquares(x,y+3*width,width,red,interval,startcolor,orientation)
-
-# ==========================================================================
-# Function to create board window
-# ==========================================================================
-
-def boardwindow():
-	global screen
-	done=1
-	screen = window(749, 468) 
-	pygame.display.set_caption("PyParty Board")
-	screen.fill(black)
-	boardsquares(150,20,50,blue,6,blue,"horizontal")
-	boardsquares(150,370,50,blue,6,blue,"horizontal")
-	boardsquares(500,20,50,green,7,green,"vertical")
-	pygame.display.flip()
-	while done:
-		for event in pygame.event.get(): # User did something
-			if event.type == pygame.QUIT: # If user clicked close
-				pygame.quit();sys.exit();
-	
-
-boardwindow()
-
-
-
-
-
-
-=======
 ##
 ### ==========================================================================			
 ### Function to create a window
@@ -200,5 +79,4 @@ boardwindow()
 ##	screen.blit(font.render(text, True, textColor), (x+20, y+7))
 ##	pygame.display.update()
 ##	return [rect, text]
->>>>>>> 0089821d6e0c9590ea30b061a7788090ac465fae
 
